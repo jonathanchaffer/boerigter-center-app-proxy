96cd5ac796e12fa7997c8f8e17341741aea3d125
jonathanchaffer <jonathanchaffer@gmail.com>
2020-11-05 11:50:29 -0500

    add localhost:3000 to whitelist

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 8080;
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 // var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
-var originWhitelist = parseEnvList("https://boerigter-center-app.web.app");
+var originWhitelist = parseEnvList("https://boerigter-center-app.web.app,https://localhost:3000");
 
 function parseEnvList(env) {
   if (!env) {
